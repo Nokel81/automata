78955ef0a3eec01974a916f1fde7c25c779117f5
Sebastian Malton <snmalton@edu.uwaterloo.ca>
2017-06-15 14:30:20 -0400

    Inital Commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const State = require('./state');
+
+/**
+ * This is the constructor for the {Automata} object
+ * @param       {Array~String} states        The list of valid state names
+ * @param       {Array~Array}  transitions   The list of valid transitions
+ *     @member {transition}
+ *     @type   {Array~Mixed}
+ *     @param  {String}      index_0 The name of the state which will be the origin of the transition
+ *     @param  {String}      index_1 The name of the state which will be the destination of the transition
+ *     @param  {function}    index_2 The accept function for a given token to take this transition
+ *         @function {index_2}
+ *         @param    {String}  token The token to be checked and if correct will the transition will be taken
+ *         @return   {Boolean}       Weather or not the token is accepted
+ * @param       {Array~String} accept_states The list of accept states
+ * @param       {String}       start_state   The name of the state to start the automata
+ * @constructor
+ */
+function Automata(states, transitions, accept_states, start_state) {
+    if (!Array.isArray(states)) {
+        throw new Error("states is not an array");
+    }
+    if (!Array.isArray(transitions)) {
+        throw new Error("transitions is not an array");
+    }
+    if (!Array.isArray(accept_states)) {
+        throw new Error("accept_states is not an array");
+    }
+    states.forEach(state => {
+        if (typeof state !== "string") {
+            throw new Error("Not every state is a string");
+        }
+    });
+    transitions.forEach(transition => {
+        if (!Array.isArray(transition)) {
+            throw new Error("Not every transition is an array");
+        }
+        if (transition.length !== 3) {
+            throw new Error("Not every transition has only a `from`, a `to` state, and a transitional token function");
+        }
+        if (states.indexOf(transition[0]) < 0 || states.indexOf(transition[1]) < 0) {
+            throw new Error("Not every transition's states are valid");
+        }
+        if (typeof transition[2] !== "function" || transition[2].length !== 1) {
+            throw new Error("Not every transition's transitional token is a function");
+        }
+    });
+    accept_states.forEach(state => {
+        if (typeof state !== "string") {
+            throw new Error("Not every accept_state is a string");
+        }
+        if (states.indexOf(state) < 0) {
+            throw new Error("Not every accept_state is a valid state");
+        }
+    });
+    if (states.indexOf(start_state) < 0) {
+        throw new Error("start_state is not a valid state");
+    }
+    let all_states = states.map(state => new State(state, accept_states));
+    all_states.forEach(state => state.setUpTransitions(all_states, transitions));
+    this.start = all_states.find(state => state.name === start_state);
+}
+
+var _ = Automata.prototype;
+
+/**
+ * This function goes through the provided tokens to check if they are valid according to the dfa
+ * @param  {Array}   tokens This is a list of tokens in forward order
+ * @param  {Function} cb     The function used to pass information back to caller
+ *     @function {cb}
+ *     @param    {Boolean} isValid            Whether or not the list of tokens is valid by the DFA
+ *     @param    {String}  error   {optional} This error string on why the validation is false
+ */
+_.validate = function (tokens, cb) {
+    this.start.transition(tokens.reverse(), cb);
+};
+
+module.exports = Automata;
---
+++ state.js
@@ -0,0 +1,49 @@
+function State(name, accept_states) {
+    if (typeof name !== "string") {
+        throw new Error("name is not a string");
+    }
+    this.name = name;
+    this.transitions = [];
+    this.isAccept = accept_states.indexOf(name) >= 0;
+}
+
+var _ = State.prototype;
+
+/**
+ * This function sets up the transitions between states after all the states have been created
+ * @param  {Array} states      The list of all the states in the Automata
+ * @param  {Array} transitions The list of valid transitions for the Automata (see {Automata} for the format)
+ */
+_.setUpTransitions = function (states, transitions) {
+    transitions.filter(transition => transition[0] === this.name)
+        .forEach(transition => this.transitions.push({
+            fn: transition[2],
+            next: states.find(state => state.name === transition[1])
+        }));
+};
+
+/**
+ * The function tries to transition to the next state given the last token in the list and the defined transitions on this state
+ * @param  {Array}    tokens List of tokens (in reverse order)
+ * @param  {Function} cb     The function that is used to send back success or failure to the caller
+ */
+_.transition = function (tokens, cb) {
+    let token = tokens.pop();
+    if (token == null) {
+        if (this.isAccept) {
+            return cb(true);
+        }
+        return cb(false, "Ended on " + this.name + " which is not an accept_state");
+    }
+    if (!this.transitions.some(transition => {
+        if (transition.fn(token)) {
+            transition.next.transition(tokens, cb);
+            return true;
+        }
+        return false;
+    })) {
+        cb(false, "Ended on " + this.name + " with " + token  + " which does not have a valid transition");
+    }
+}
+
+module.exports = State;
